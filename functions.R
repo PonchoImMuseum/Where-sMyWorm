d1f0dc1912dbe5381e562cb67b67746505f4777b
PonchoAceves <bioarach2@gmail.com>
2025-07-09 13:43:13 +0200

    Before applying Claude's solution for the dynamic reading of names in data for the reactable function that takes full catalogNumbers

--- functions.R
+++ functions.R
@@ -188,6 +188,85 @@ display_catalog_around_match <- function(data, input_num) {
 }
 
 # NEW FUNCTION: Display catalog around match using full catalog number
+# display_catalog_around_full_match <- function(data, catalog_number) {
+#   # data must already have row_id column
+#   
+#   matches <- find_exact_catalog_match(data, catalog_number)
+#   
+#   if (nrow(matches) == 0) stop("No matches found for catalog number: ", catalog_number)
+#   
+#   matched_row <- matches %>% slice(1)
+#   
+#   rows_before <- matched_row$rows_before
+#   rows_after <- matched_row$rows_after
+#   
+#   smaller_count <- min(rows_before, rows_after)
+#   if (is.na(smaller_count)) smaller_count <- 0
+#   
+#   if (smaller_count < 5) {
+#     if (rows_before <= rows_after) {
+#       display_before <- rows_before
+#       display_after <- 10
+#     } else {
+#       display_before <- 10
+#       display_after <- rows_after
+#     }
+#   } else {
+#     if (rows_before <= rows_after) {
+#       display_before <- smaller_count
+#       display_after <- 5
+#     } else {
+#       display_before <- 5
+#       display_after <- smaller_count
+#     }
+#   }
+#   
+#   start_row <- max(matched_row$row_id - display_before, 1)
+#   end_row <- min(matched_row$row_id + display_after, nrow(data))
+#   
+#   subset_df <- data %>%
+#     filter(row_id >= start_row & row_id <= end_row) %>%
+#     mutate(
+#       is_matched = (row_id == matched_row$row_id),
+#       neighbours = row_number() - (display_before + 1)
+#     ) %>%
+#     select(-row_id)
+#   
+#   css <- "
+#   .highlighted-row {
+#     background-color: #fff9c4;
+#   }
+#   "
+#   
+#   reactable(
+#     subset_df,
+#     columns = list(
+#       is_matched = colDef(show = FALSE),
+#       neighbours = colDef(name = "Neighbours", align = "center", width = 80)
+#     ),
+#     rowClass = function(index) {
+#       if (subset_df$is_matched[index]) "highlighted-row" else NULL
+#     },
+#     highlight = TRUE,
+#     bordered = TRUE,
+#     striped = TRUE,
+#     pagination = FALSE,
+#     height = 600,
+#     width = 1200
+#   ) %>%
+#     htmltools::browsable() %>%
+#     htmltools::tagList(
+#       htmltools::tags$style(css)
+#     )
+# }
+
+# Example usage:
+# df <- read_csv("combined_with_shelving.csv") %>% mutate(row_id = row_number())
+# matches <- find_catalog_matches_map(df, "0089")
+# display_catalog_around_match(df, "089")
+# display_catalog_around_full_match(df, "ZMH-ANN-PE0078") %>% browsable()
+
+# NEW FUNCTION: Display catalog around match using full catalog number
 display_catalog_around_full_match <- function(data, catalog_number) {
   # data must already have row_id column
   
@@ -232,36 +311,42 @@ display_catalog_around_full_match <- function(data, catalog_number) {
     ) %>%
     select(-row_id)
   
-  css <- "
-  .highlighted-row {
-    background-color: #fff9c4;
-  }
-  "
-  
+  # Return reactable directly for Shiny use (remove browsable() and tagList())
   reactable(
     subset_df,
     columns = list(
       is_matched = colDef(show = FALSE),
-      neighbours = colDef(name = "Neighbours", align = "center", width = 80)
+      neighbours = colDef(name = "Neighbours", align = "center", width = 80),
+      catalogNumber = colDef(name = "Catalog Number"),
+      FAMILY = colDef(name = "Family"),
+      GENUS = colDef(name = "Genus"),
+      SPECIES = colDef(name = "Species"),
+      SUBSPECIES = colDef(name = "Subspecies"),
+      aisle = colDef(name = "Aisle"),
+      shelving_unit = colDef(name = "Shelving Unit"),
+      shelf = colDef(name = "Shelf"),
+      rows_before = colDef(name = "Rows Before"),
+      rows_after = colDef(name = "Rows After")
     ),
-    rowClass = function(index) {
-      if (subset_df$is_matched[index]) "highlighted-row" else NULL
+    rowStyle = function(index) {
+      if (subset_df$is_matched[index]) {
+        list(backgroundColor = "#fff9c4", color = "black")
+      } else if (index %% 2 == 0) {
+        list(backgroundColor = "#2F6FD9", color = "white")
+      } else {
+        list(backgroundColor = "#3A87FE", color = "white")
+      }
     },
     highlight = TRUE,
     bordered = TRUE,
     striped = TRUE,
     pagination = FALSE,
     height = 600,
-    width = 1200
-  ) %>%
-    htmltools::browsable() %>%
-    htmltools::tagList(
-      htmltools::tags$style(css)
+    width = 1200,
+    style = list(
+      backgroundColor = "#3A87FE",
+      color = "white"
     )
+  )
 }
 
-# Example usage:
-# df <- read_csv("combined_with_shelving.csv") %>% mutate(row_id = row_number())
-# matches <- find_catalog_matches_map(df, "0089")
-# display_catalog_around_match(df, "089")
-# display_catalog_around_full_match(df, "ZMH-ANN-PE0078")
